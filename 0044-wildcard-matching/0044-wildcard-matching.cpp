a31daaf376d8a25f0338f6a531560391dbeabce7
MD JAMI KHAN <118730471+mdjamikhan@users.noreply.github.com>
2024-04-17 22:22:57 +0530

    Time: 70 ms (32.87%), Space: 15.6 MB (75.87%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -20,25 +20,33 @@ int solve(int i,int j,string &s,string &p,vector<vector<int>>&dp){
     bool isMatch(string s, string p) {
         int n=s.size();
         int m=p.size();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return solve(n-1,m-1,s,p,dp);
-
-
-
-        // for(int i=0;i<=n;i++){
-        //     dp[i][0]=0;
-        // }
-        // for(int i=1;i<=m;i++)
-        // {
-        //     dp[0][i]=0;
-        // }
-        // for(int i=1;i<=n;i++){
-        //     for(int j=1;j<=m;j++)
-        //     {
-        //         if(s[i-1]==t[j-1])dp[i][j]=1+dp[i-1][j-1];
-        //         else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-        //     }
-        // }
         
+       // vector<vector<int>>dp(n,vector<int>(m,-1));
+       // return solve(n-1,m-1,s,p,dp);
+
+         vector<vector<bool>>dp(n+1,vector<bool>(m+1,false));
+         dp[0][0] =true;
+
+
+
+
+        for(int i=1;i<=m;i++){
+           if(p[i-1]=='*'){
+            dp[0][i]=dp[0][i-1];
+            }
+            else{
+                break;
+            }
+            
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++)
+            {
+                if(s[i-1]==p[j-1] || p[j-1]=='?')dp[i][j]=dp[i-1][j-1];
+                else if(p[j-1]=='*') dp[i][j]=dp[i][j-1]||dp[i-1][j];
+            
+            }
+        }
+        return dp[n][m];
     }
 };
