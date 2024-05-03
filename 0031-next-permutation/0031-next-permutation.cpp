6c304ca22da41ae6cc5830c166006addc8893f5f
MD JAMI KHAN <118730471+mdjamikhan@users.noreply.github.com>
2024-05-04 00:52:22 +0530

    Time: 4 ms (58.34%), Space: 14.7 MB (15.38%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n=nums.size();
+        int index=-1;
+        for(int i=n-2;i>=0;i--)
+        {
+            if(nums[i]<nums[i+1]){
+                index=i;
+                break;
+            }
+
+        }
+        if(index==-1){
+            reverse(nums.begin(),nums.end());
+            return;
+        }
+       
+            for(int i=n-1;i>=0;i--)
+            {
+                if(nums[i]>nums[index]){
+                    swap(nums[index],nums[i]);
+                    break;
+                }
+            }
+            reverse(nums.begin()+index+1,nums.end());
+        
+
+
+        //next_permutation(nums.begin(),nums.end());
+        
+    }
+};
