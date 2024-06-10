800b96df68000f503a0f049b8ef095bf58c3b750
MD JAMI KHAN <118730471+mdjamikhan@users.noreply.github.com>
2024-06-10 12:59:20 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3175-find-the-first-player-to-win-k-games-in-a-row/3175-find-the-first-player-to-win-k-games-in-a-row.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int findWinningPlayer(vector<int>& skills, int k) {
+
+        int n=skills.size();
+        int count=0;
+        int index=0;
+        if(k>=n){
+            for(int i=0;i<n;i++)
+            {
+                if(skills[index]<skills[i]){
+                    index=i;
+                }
+            }
+            return index;
+        }
+        
+        for(int i=1;i<n-1;i++)
+        {
+            if(skills[index]>skills[i]){
+                count++;   
+            }
+            else{
+                index=i;
+                count=0;
+            }
+
+            if(count==k) break;
+
+
+        }
+        return index;
+
+        
+    }
+};
