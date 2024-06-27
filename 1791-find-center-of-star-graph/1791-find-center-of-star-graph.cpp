97ccaa8e6b5d3b6dcec9fa84e183857dd9179fd6
MD JAMI KHAN <118730471+mdjamikhan@users.noreply.github.com>
2024-06-27 10:12:44 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
@@ -3,23 +3,26 @@ public:
     int findCenter(vector<vector<int>>& edges) {
         int n=edges.size();
         int k=0;
-        unordered_map<int,int>mp;
+        if(edges[0][1]==edges[1][0] )return edges[0][1];
+        return edges[1][1];
+
+        // unordered_map<int,int>mp;
         
-        for(auto it:edges)
-        {
-            mp[it[0]]++;
-            mp[it[1]]++;
+        // for(auto it:edges)
+        // {
+        //     mp[it[0]]++;
+        //     mp[it[1]]++;
 
 
-        }
-        int maxi=0;
-        for(auto it:mp){
-            if(it.second>maxi){
-                maxi=it.second;
-                k=it.first;
-            }
+        // }
+        // int maxi=0;
+        // for(auto it:mp){
+        //     if(it.second>maxi){
+        //         maxi=it.second;
+        //         k=it.first;
+        //     }
 
-        }
+        // }
         return k;
         
     }
