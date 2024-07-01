01afed350470477477e69d29c56eff99223b61cc
MD JAMI KHAN <118730471+mdjamikhan@users.noreply.github.com>
2024-07-01 09:17:28 +0530

    Time: 6 ms (25.2%), Space: 10.4 MB (99.07%) - LeetHub

---
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        int count=0;
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]&1) count++;
+            else count=0;
+
+            if(count==3) return true;
+        }
+        return false;
+        
+    }
+};
