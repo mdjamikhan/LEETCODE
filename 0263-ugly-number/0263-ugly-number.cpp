14b99b147acea18715967227df7e8527db353680
MD JAMI KHAN <118730471+mdjamikhan@users.noreply.github.com>
2024-08-18 10:18:10 +0530

    Time: 3 ms (26.42%), Space: 7.6 MB (9.61%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isUgly(int n) {
+        vector<int>ans;
+        if(n==0) return false;
+
+        while(n%2==0){
+            n/=2;
+        }
+        while(n%3==0){
+            n/=3;
+        }
+        while(n%5==0){
+            n/=5;
+        }
+        cout<<n<<endl;
+        if(n!=1) return false;
+        return true;
+
+
+
+        
+    }
+};
