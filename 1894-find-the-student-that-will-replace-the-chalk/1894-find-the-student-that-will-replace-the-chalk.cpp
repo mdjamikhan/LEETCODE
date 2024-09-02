2be786255eef0ef6a2180f627cbaacb853fc8ca1
MD JAMI KHAN <118730471+mdjamikhan@users.noreply.github.com>
2024-09-02 20:15:09 +0530

    Time: 87 ms (85.34%), Space: 77.2 MB (8.71%) - LeetHub

---
+++ 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int chalkReplacer(vector<int>& chalk, int k) {
+       long long sum= accumulate(chalk.begin(), chalk.end(), 0LL);
+       k=k%sum;
+
+       for(int i=0;i<chalk.size();i++)
+       {
+            if(chalk[i]>k) return i;
+
+            k-=chalk[i];
+
+
+       }
+       
+        return -1;
+        
+    }
+};
