238d2e9ae991c6f2c732388c6b378c5f2dd3d845
MD JAMI KHAN <118730471+mdjamikhan@users.noreply.github.com>
2024-09-04 23:11:03 +0530

    Time: 6 ms (70.22%), Space: 11.2 MB (78.78%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -3,7 +3,7 @@ public:
 int solve(int i,string &s,vector<string>&word,vector<int>&dp,set<string>&st){
     if(i==s.size()) return 1;
 
-   // if(dp[i]!=-1) return dp[i];
+    if(dp[i]!=-1) return dp[i];
 
     string temp="";
     for(int k=i;k<s.size();k++)
@@ -11,11 +11,11 @@ int solve(int i,string &s,vector<string>&word,vector<int>&dp,set<string>&st){
         temp+=s[k];
         if(st.find(temp)!=st.end()){
             
-          //  dp[i]=1;
+          dp[i]=1;
             if(solve(k+1,s,word,dp,st)) return 1;
         }
     }
-    return 0;
+    return dp[i]=0;
 }
 
     bool wordBreak(string s, vector<string>& word) {
