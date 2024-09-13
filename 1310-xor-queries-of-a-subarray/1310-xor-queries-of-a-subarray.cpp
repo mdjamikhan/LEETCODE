8bd6e9bdff927cb464cc88e5e0b1a3880b22aa37
MD JAMI KHAN <118730471+mdjamikhan@users.noreply.github.com>
2024-09-13 13:50:08 +0530

    Time: 55 ms (78.82%), Space: 42.1 MB (53.29%) - LeetHub

--- 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.cpp
+++ 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.cpp
@@ -2,18 +2,25 @@ class Solution {
 public:
     vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& qu) {
         vector<int>ans;
+        vector<int>store(arr.size());
+        store[0]=arr[0];
+        for(int i=1;i<arr.size();i++)
+        {
+            store[i]=store[i-1]^arr[i];
+
+            
+        }
         for(int i=0;i<qu.size();i++)
         {
-            int one=qu[i][0];
-            int two=qu[i][1];
-            int sum=0;
-            for(int j=one;j<=two;j++)
-            {
-                sum^=arr[j];
-
+            int left=qu[i][0];
+            int right=qu[i][1];
+            if(left==0){
+                ans.push_back(store[right]);
             }
-            ans.push_back(sum);
-            
+            else{
+                ans.push_back(store[right]^store[left-1]);
+            }
+
         }
         return ans;
         
