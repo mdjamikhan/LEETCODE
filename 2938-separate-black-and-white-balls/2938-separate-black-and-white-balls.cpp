105acc9969a12d3f9161be2460cd804ef21f27b2
MD JAMI KHAN <118730471+mdjamikhan@users.noreply.github.com>
2024-10-15 09:40:13 +0530

    Time: 36 ms (32%), Space: 15.6 MB (45.68%) - LeetHub

--- 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.cpp
+++ 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     long long minimumSteps(string s) {
-        int i=0;
-        int j=s.size();
         long long count=0;
-        long long  one=0;
-        while(i<j){
-
-            if(s[i]=='0') count+=one;  
-            if(s[i]=='1') one++;
-            i++;
-            
+        long long zero=0;
+        int i=0;
+        int j=s.size()-1;
+        while(i<=j){
+            if(s[j]=='1') count+=zero;
+            if(s[j]=='0') zero++;
+            j--;
         }
         return count;
-
-    }
         
-    
+    }
 };
