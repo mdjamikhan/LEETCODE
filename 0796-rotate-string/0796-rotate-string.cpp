11556aae5c8a02b372758ed79b59e01f59579e60
MD JAMI KHAN <118730471+mdjamikhan@users.noreply.github.com>
2024-11-03 11:06:56 +0530

    Time: 3 ms (7.41%), Space: 7.7 MB (55.81%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -2,28 +2,9 @@ class Solution {
 public:
     bool rotateString(string s, string goal) {
         if(s.size()!=goal.size()) return false;
-        int index=-1;
-        for(int i=0;i<goal.size();i++){
-            if(s[0]==goal[i]){
-                index=i;
+        string temp=goal+goal;
+        return temp.find(s)!=string ::npos;
 
-            }
-
-        }
-        string a="";
-        for(int i=index;i<goal.size();i++)
-        {
-            a+=goal[i];
-
-
-        }
-        for(int i=0;i<index;i++)
-        {
-            a+=goal[i];
-
-        }
-        cout<<a;
-        return a==s;
 
 
         
