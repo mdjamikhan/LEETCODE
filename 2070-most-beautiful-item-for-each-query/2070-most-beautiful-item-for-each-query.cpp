223fa8d5578fdfc989358b7387687a470f1a8e6f
MD JAMI KHAN <118730471+mdjamikhan@users.noreply.github.com>
2024-11-12 13:06:19 +0530

    Time: 48 ms (78.69%), Space: 94.6 MB (58.11%) - LeetHub

--- 2070-most-beautiful-item-for-each-query/2070-most-beautiful-item-for-each-query.cpp
+++ 2070-most-beautiful-item-for-each-query/2070-most-beautiful-item-for-each-query.cpp
@@ -2,44 +2,42 @@ class Solution {
 public:
     vector<int> maximumBeauty(vector<vector<int>>& it, vector<int>& qu) {
 
+        sort(begin(it),end(it));
 
-   sort(it.begin(), it.end());
-//    sort(qu.begin(), qu.end());
-    vector<pair<int, int>> store;
-    store.push_back({it[0][0],it[0][1]});
-
-    for (int i = 1; i < it.size(); i++) {  
-        if (it[i][0] == store.back().first) {
-            if (it[i][1] > store.back().second) {
+        vector<pair<int,int>>store;
+        store.push_back({it[0][0],it[0][1]});
+        for(int i=1;i<it.size();i++){
+            if(it[i][0]==store.back().first){
                 store.back().second=max(it[i][1],store.back().second);
             }
-        } else {
-            store.push_back({it[i][0], it[i][1]});
-        }
-    }
-    vector<int>ans;
-    // for (int i = 0; i < store.size(); i++) {
-    //     cout << store[i].first << " " << store[i].second << endl;
-    // }
-
-    for(int i=0;i<qu.size();i++){
-        int maxi=0;
-        int checkPoint=qu[i];
-        for(auto it:store){
-            if(checkPoint>=it.first){
-                maxi=max(maxi,it.second);
-            }
             else{
-                break;
+                store.push_back({it[i][0],it[i][1]});
             }
         }
-        ans.push_back(maxi);
+        // now main part of this question it basically an edge case of question
+        // we have to take maxi of neighbour element;
+        for(int i=1;i<store.size();i++){
+            store[i].second=max(store[i-1].second,store[i].second);
+        }
+        vector<int>ans;
         
-    }
-    return ans;
-            
-
-
-        
+        for(int it:qu){
+            int checkPoint=it;
+            int low=0;
+            int maxi=0;
+            int high=store.size()-1;
+            while(low<=high){
+                int mid=low+(high-low)/2;
+                if(store[mid].first<=checkPoint){
+                    maxi=store[mid].second;
+                    low=mid+1;
+                }
+                else{
+                    high=mid-1;
+                }
+            }
+            ans.push_back(maxi);
+        }
+        return ans;
     }
 };
