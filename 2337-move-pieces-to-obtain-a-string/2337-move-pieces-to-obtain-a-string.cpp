d0c93316343883e4ee125c685c0993cd995ab37d
MD JAMI KHAN <118730471+mdjamikhan@users.noreply.github.com>
2024-12-05 13:19:33 +0530

    Time: 1661 ms (5.17%), Space: 21.8 MB (35.59%) - LeetHub

---
+++ 2337-move-pieces-to-obtain-a-string/2337-move-pieces-to-obtain-a-string.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool canChange(string start, string target) {
+        int n=start.size();
+        int i=0;
+        int j=0;
+   
+        while(i<n || j<n){
+            while(i<n && start[i]=='_') i++;
+            while(j<n && target[j]=='_') j++;
+
+            cout<<i<<" "<<j<<" ";
+
+            if(i==n && j==n) return true;
+            if(i==n || j==n) return false;
+            if(start[i]!=target[j]) return false;
+
+            if (start[i] == 'L' && i < j) return false;
+            if (start[i] == 'R' && i > j) return false;
+            i++;
+            j++;
+            
+        }
+        return true;
+        
+    }
+};
