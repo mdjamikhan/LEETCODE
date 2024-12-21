4cbd885ac82d0716810a53cbe5d6d251f3eef14d
MD JAMI KHAN <118730471+mdjamikhan@users.noreply.github.com>
2024-12-21 14:48:04 +0530

    Time: 0 ms (100%), Space: 10.2 MB (12.68%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -6,7 +6,7 @@ public:
 
         for(int i=0;i<n;i++)
         {
-            for(int j=0;j<=i;j++)
+            for(int j=i;j<m;j++)
             {
                // matrix[i][j]=matrix[j][i];
                 swap(matrix[j][i],matrix[i][j]);
